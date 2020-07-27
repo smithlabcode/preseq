cf751f71c183c8ffae3ebf7701a5118d046cf9fb
Andrew D Smith <andrewds@usc.edu>
2020-07-27 10:58:10 -0700

    Cleaning up some code

--- src/moment_sequence.cpp
+++ src/moment_sequence.cpp
@@ -34,6 +34,7 @@
 #include <iomanip>
 #include <iostream>
 #include <cassert>
+#include <algorithm>
 
 using std::string;
 using std::vector;
@@ -41,6 +42,9 @@ using std::endl;
 using std::max;
 using std::cerr;
 using std::setprecision;
+using std::swap;
+using std::find_if;
+using std::transform;
 
 
 /////////////////////////////////////////////////////
@@ -49,58 +53,54 @@ using std::setprecision;
 // truncate moment sequence to ensure pos def
 size_t
 ensure_pos_def_mom_seq(vector <double> &moments,
-		       const double tolerance,
-		       const bool VERBOSE){
+                       const double tolerance,
+                       const bool VERBOSE) {
 
   const size_t min_hankel_dim = 1;
   size_t hankel_dim = 2;
-  if(moments.size() < 2*hankel_dim){
-    if(VERBOSE)
+  if (moments.size() < 2*hankel_dim) {
+    if (VERBOSE)
       cerr << "too few moments" << endl;
     return min_hankel_dim;
   }
 
   bool ACCEPT_HANKEL = true;
-  while(ACCEPT_HANKEL && 
-	(2*hankel_dim - 1 < moments.size())){
-    gsl_matrix *hankel_matrix = gsl_matrix_alloc(hankel_dim, 
-						 hankel_dim);
-    for(size_t col_indx = 0; col_indx < hankel_dim; col_indx++){
-      for(size_t row_indx = 0; row_indx < hankel_dim; row_indx++){
-	gsl_matrix_set(hankel_matrix, col_indx, row_indx, 
-		       moments[col_indx + row_indx]);
-      }
-    }
-    int s;
+  while (ACCEPT_HANKEL && (2*hankel_dim - 1 < moments.size())) {
+
+    gsl_matrix *hankel_mat = gsl_matrix_alloc(hankel_dim, hankel_dim);
+    for (size_t c_idx = 0; c_idx < hankel_dim; c_idx++)
+      for (size_t r_idx = 0; r_idx < hankel_dim; r_idx++)
+        gsl_matrix_set(hankel_mat, c_idx, r_idx, moments[c_idx + r_idx]);
+
+    int s = 0;
     gsl_permutation *perm = gsl_permutation_alloc(hankel_dim);
-    gsl_linalg_LU_decomp(hankel_matrix, perm, &s);
-    double hankel_matrix_det = gsl_linalg_LU_det(hankel_matrix, s);
+    gsl_linalg_LU_decomp(hankel_mat, perm, &s);
+    const double hankel_mat_det = gsl_linalg_LU_det(hankel_mat, s);
 
-    gsl_matrix *shifted_hankel_matrix = gsl_matrix_alloc(hankel_dim, 
-						 hankel_dim);
-    for(size_t col_indx = 0; col_indx < hankel_dim; col_indx++){
-      for(size_t row_indx = 0; row_indx < hankel_dim; row_indx++){
-	gsl_matrix_set(shifted_hankel_matrix, col_indx, row_indx, 
-		       moments[col_indx + row_indx + 1]);
-      }
-    }
+    gsl_matrix *shift_hankel_mat = gsl_matrix_alloc(hankel_dim, hankel_dim);
+    for (size_t c_idx = 0; c_idx < hankel_dim; c_idx++)
+      for (size_t r_idx = 0; r_idx < hankel_dim; r_idx++)
+        gsl_matrix_set(shift_hankel_mat, c_idx, r_idx,
+                       moments[c_idx + r_idx + 1]);
+
     gsl_permutation *s_perm = gsl_permutation_alloc(hankel_dim);
-    gsl_linalg_LU_decomp(shifted_hankel_matrix, s_perm, &s);
-    double shifted_hankel_matrix_det = gsl_linalg_LU_det(shifted_hankel_matrix, s);
+    gsl_linalg_LU_decomp(shift_hankel_mat, s_perm, &s);
+    const double shift_hankel_mat_det = gsl_linalg_LU_det(shift_hankel_mat, s);
 
-    if(VERBOSE){
-      cerr << "dim" << '\t' << "hankel_det" << '\t' << "shifted_hankel_det" << endl;
-      cerr << hankel_dim << '\t' << hankel_matrix_det   
-	   << '\t' << shifted_hankel_matrix_det << endl;
+    if (VERBOSE) {
+      cerr << "dim" << '\t'
+           << "hankel_det" << '\t'
+           << "shifted_hankel_det" << endl;
+      cerr << hankel_dim << '\t'
+           << hankel_mat_det << '\t'
+           << shift_hankel_mat_det << endl;
     }
-  
 
-    if(hankel_matrix_det > tolerance &&
-       shifted_hankel_matrix_det > tolerance){
+    if (hankel_mat_det > tolerance && shift_hankel_mat_det > tolerance) {
       ACCEPT_HANKEL = true;
       hankel_dim++;
     }
-    else{
+    else {
       ACCEPT_HANKEL = false;
       hankel_dim--;
       moments.resize(2*hankel_dim);
@@ -118,34 +118,34 @@ ensure_pos_def_mom_seq(vector <double> &moments,
 // check 3 term recurrence to avoid non-positive elements
 // truncate if non-positive element found
 static void
-check_three_term_relation(vector<double> &a,
-			  vector<double> &b){
+check_three_term_relation(vector<double> &a, vector<double> &b) {
 
-  // first entry is zero! Abort
-  if(a[0] <= 0.0){
+  // abort if first entry is zero or smaller
+  if (a[0] <= 0.0) {
     a.clear();
     b.clear();
   }
 
-  for(size_t i = 0; i < b.size(); i++){
-    if(b[i] <= 0.0 || !isfinite(b[i])
-       || a[i + 1] <= 0.0 || !isfinite(a[i + 1])){
+  for (size_t i = 0; i < b.size(); i++)
+    // ADS: some strange logic here
+    if (b[i] <= 0.0 || !isfinite(b[i]) ||
+        a[i + 1] <= 0.0 || !isfinite(a[i + 1])) {
       b.resize(i);
       a.resize(i + 1);
       break;
     }
-  }
 }
 
-// check moment_sequence to avoid non-positive elements
-// truncate if non-positive element found
+// check the moment sequence to avoid non-positive elements and
+// truncate at first non-positive element if found
 static void
-check_moment_sequence(vector<double> &obs_moms){
-  if(obs_moms[0] <= 0.0 || !isfinite(obs_moms[0]))
-     obs_moms.clear();
+check_moment_sequence(vector<double> &obs_moms) {
 
-  for(size_t i = 1; i < obs_moms.size(); i++){
-    if(obs_moms[i] <= 0.0 || !isfinite(obs_moms[i])){
+  if (obs_moms[0] <= 0.0 || !isfinite(obs_moms[0]))
+    obs_moms.clear();
+
+  for (size_t i = 1; i < obs_moms.size(); i++) {
+    if (obs_moms[i] <= 0.0 || !isfinite(obs_moms[i])) {
       obs_moms.resize(i + 1);
       break;
     }
@@ -154,7 +154,7 @@ check_moment_sequence(vector<double> &obs_moms){
 
 
 void
-MomentSequence::unmodified_Chebyshev(const bool VERBOSE){
+MomentSequence::unmodified_Chebyshev(const bool VERBOSE) {
 
   const size_t n_points = static_cast<size_t>(floor(moments.size()/2));
   vector<double> a(n_points, 0.0);
@@ -164,20 +164,20 @@ MomentSequence::unmodified_Chebyshev(const bool VERBOSE){
   // initialization
   a[0] = moments[1]/moments[0];
   // sigma[-1][l] = 0
-  for(size_t l = 0; l < 2*n_points; l++)
+  for (size_t l = 0; l < 2*n_points; l++)
     sigma[0][l] = moments[l];
 
-  for(size_t k = 1; k <= n_points; k++){
-    for(size_t l = k; l < 2*n_points - k; l++){
+  for (size_t k = 1; k <= n_points; k++) {
+    for (size_t l = k; l < 2*n_points - k; l++) {
       sigma[k][l] = sigma[k-1][l+1] - a[k-1]*sigma[k-1][l];
-      if(k > 1)
-	sigma[k][l] -= b[k-2]*sigma[k-2][l];
+      if (k > 1)
+        sigma[k][l] -= b[k-2]*sigma[k-2][l];
     }
-    if(k != n_points){
+    if (k != n_points) {
       a[k] = sigma[k][k+1]/sigma[k][k] - sigma[k-1][k]/sigma[k-1][k-1];
       b[k-1] = sigma[k][k]/sigma[k-1][k-1];
     }
-  }  
+  }
 
   alpha = a;
   beta = b;
@@ -186,8 +186,8 @@ MomentSequence::unmodified_Chebyshev(const bool VERBOSE){
 // un-normalized 3 term recurrence
 void
 MomentSequence::full_3term_recurrence(const bool VERBOSE,
-				      vector<double> &full_alpha,
-				      vector<double> &full_beta){
+                                      vector<double> &full_alpha,
+                                      vector<double> &full_beta) {
 
   const size_t n_points = static_cast<size_t>(floor(moments.size()/2));
   vector<double> a(n_points, 0.0);
@@ -197,20 +197,20 @@ MomentSequence::full_3term_recurrence(const bool VERBOSE,
   // initialization
   a[0] = moments[1]/moments[0];
   // sigma[-1][l] = 0
-  for(size_t l = 0; l < 2*n_points; l++)
+  for (size_t l = 0; l < 2*n_points; l++)
     sigma[0][l] = moments[l];
 
-  for(size_t k = 1; k <= n_points; k++){
-    for(size_t l = k; l < 2*n_points - k; l++){
+  for (size_t k = 1; k <= n_points; k++) {
+    for (size_t l = k; l < 2*n_points - k; l++) {
       sigma[k][l] = sigma[k-1][l+1] - a[k-1]*sigma[k-1][l];
-      if(k > 1)
-	sigma[k][l] -= b[k-2]*sigma[k-2][l];
+      if (k > 1)
+        sigma[k][l] -= b[k-2]*sigma[k-2][l];
     }
-    if(k != n_points){
+    if (k != n_points) {
       a[k] = sigma[k][k+1]/sigma[k][k] - sigma[k-1][k]/sigma[k-1][k-1];
       b[k-1] = sigma[k][k]/sigma[k-1][k-1];
     }
-  }  
+  }
 
   full_alpha.swap(a);
   full_beta.swap(b);
@@ -235,14 +235,14 @@ MomentSequence::MomentSequence(const vector<double> &obs_moms) :
 /////////////////////////////////////////////////////
 // Quadrature Methods
 
-// one iteration of QR: 
+// one iteration of QR:
 // following eq's 3.3 of Golub & Welsh
 // one iteration is Z_N-1*Z_N-2*...*Z_1*X*Z_1*...*Z_N-1
 // Z_j is givens matrix to zero out the j+1,j'th element of X
 static void
-QRiteration(vector<double> &alpha,
-	    vector<double> &beta,
-	    vector<double> &weights){
+QRiteration(vector<double> &alpha, vector<double> &beta,
+            vector<double> &weights) {
+
   // initialize variables
   vector<double> sin_theta(alpha.size(), 0.0);
   vector<double> cos_theta(alpha.size(), 0.0);
@@ -264,10 +264,10 @@ QRiteration(vector<double> &alpha,
   vector<double> z_bar(weights.size(), 0.0);
   z_bar[0] = z[0];
 
+  for (size_t j = 0; j < alpha.size() - 1; j++) {
 
-  for(size_t j = 0; j < alpha.size() - 1; j++){
-    // for d and b_bar, j here is j-1 in G&W 
-    if(d[j] == 0.0 && b_bar[j] == 0.0){
+    // for d and b_bar, j here is j-1 in G&W
+    if (d[j] == 0.0 && b_bar[j] == 0.0) {
       sin_theta[j] = 0.0;
       cos_theta[j] = 1.0;
     }
@@ -276,22 +276,23 @@ QRiteration(vector<double> &alpha,
       cos_theta[j] = b_bar[j]/sqrt(d[j]*d[j] + b_bar[j]*b_bar[j]);
     }
 
-    a[j] = a_bar[j]*cos_theta[j]*cos_theta[j]
-      + 2*b_tilde[j]*cos_theta[j]*sin_theta[j] 
-      + alpha[j+1]*sin_theta[j]*sin_theta[j];
+    a[j] = (a_bar[j]*cos_theta[j]*cos_theta[j] +
+            2*b_tilde[j]*cos_theta[j]*sin_theta[j] +
+            alpha[j+1]*sin_theta[j]*sin_theta[j]);
 
-    a_bar[j+1] = a_bar[j]*sin_theta[j]*sin_theta[j]
-      - 2*b_tilde[j]*cos_theta[j]*sin_theta[j] 
-      + alpha[j+1]*cos_theta[j]*cos_theta[j];
+    a_bar[j+1] = (a_bar[j]*sin_theta[j]*sin_theta[j] -
+                  2*b_tilde[j]*cos_theta[j]*sin_theta[j] +
+                  alpha[j+1]*cos_theta[j]*cos_theta[j]);
 
-    if(j != 0)
+    if (j != 0)
       b[j-1] = sqrt(d[j]*d[j] + b_bar[j]*b_bar[j]);
 
-    b_bar[j+1] = (a_bar[j] - alpha[j+1])*sin_theta[j]*cos_theta[j]
-      + b_tilde[j]*(sin_theta[j]*sin_theta[j] - cos_theta[j]*cos_theta[j]);
+    b_bar[j+1] =
+      ((a_bar[j] - alpha[j+1])*sin_theta[j]*cos_theta[j] +
+       b_tilde[j]*(sin_theta[j]*sin_theta[j] - cos_theta[j]*cos_theta[j]));
 
     b_tilde[j+1] = -beta[j+1]*cos_theta[j];
-    
+
     d[j+1] = beta[j+1]*sin_theta[j];
 
     z[j] = z_bar[j]*cos_theta[j] + weights[j+1]*sin_theta[j];
@@ -299,33 +300,31 @@ QRiteration(vector<double> &alpha,
     z_bar[j+1] = z_bar[j]*sin_theta[j] - weights[j+1]*cos_theta[j];
   }
 
-// last entries set equal to final "holding" values
+  // last entries set equal to final "holding" values
   a.back() = a_bar.back();
   b.back() = b_bar.back();
   z.back() = z_bar.back();
 
-  alpha.swap(a);
-  beta.swap(b);
-  weights.swap(z);
+  swap(alpha, a);
+  swap(beta, b);
+  swap(weights, z);
 }
 
-static bool
-check_positivity(const vector<double> &points){
-  for(size_t i = 0; i < points.size(); i++)
-    if(points[i] <= 0.0 || !isfinite(points[i]))
-      return false;
 
-  return true;
+static bool
+check_positivity(const vector<double> &v) {
+  return find_if(begin(v), end(v),
+                 [](const double x) {return x <= 0.0 || isinf(x);}) == end(v);
 }
 
 
 bool
 MomentSequence::Lower_quadrature_rules(const bool VERBOSE,
-				       const size_t n_points,
-				       const double tol, 
-				       const size_t max_iter,
-				       vector<double> &points,
-				       vector<double> &weights){
+                                       const size_t n_points,
+                                       const double tol,
+                                       const size_t max_iter,
+                                       vector<double> &points,
+                                       vector<double> &weights) {
 
   // make sure that points.size() will be less than n_points
   vector<double> a(alpha);
@@ -338,75 +337,39 @@ MomentSequence::Lower_quadrature_rules(const bool VERBOSE,
   // See Gautschi pgs 10-13,
   // the nu here is the square of the off-diagonal
   // of the Jacobi matrix
-  for(size_t i = 0; i < b.size(); i++)
+  for (size_t i = 0; i < b.size(); i++)
     b[i] = sqrt(b[i]);
-
-  /*
-  if(VERBOSE){
-    for(size_t i = 0; i < a.size(); i++)
-      cerr << "alpha_" << i << '\t';
-    cerr << endl;
-    for(size_t i = 0; i < a.size(); i++)
-      cerr << a[i] << '\t';
-    cerr << endl;
-
-    for(size_t i = 0; i < b.size(); i++)
-      cerr << "beta_" << i << '\t';
-    cerr << endl;
-    for(size_t i = 0; i < b.size(); i++)
-      cerr << b[i] << '\t';
-    cerr << endl;
-  }
-  */
 
   vector<double> eigenvec(a.size(), 0.0);
   eigenvec[0] = 1.0;
   vector<double> eigenvals(a);
   vector<double> qr_beta(b);
-  // in QR, off-diagonals go to zero
-  // use off diags for convergence
-  double error = 0.0;
-  for(size_t i = 0; i < qr_beta.size(); i++)
-    error += fabs(qr_beta[i]);
+
+  // in QR, off-diagonals go to zero use off diags for convergence
+  double error_sum = 0.0;
+  for (size_t i = 0; i < qr_beta.size(); i++)
+    error_sum += fabs(qr_beta[i]);
+
   size_t iter = 0;
-  while(iter < max_iter && error > tol){
+  while (iter < max_iter && error_sum > tol) {
     QRiteration(eigenvals, qr_beta, eigenvec);
 
-    error = 0.0;
-    for(size_t i = 0; i < qr_beta.size(); i++)
-      error += fabs(qr_beta[i]);
+    error_sum = 0.0;
+    for (size_t i = 0; i < qr_beta.size(); i++)
+      error_sum += fabs(qr_beta[i]);
     iter++;
-
   }
+
   // eigenvalues are on diagonal of J
-  bool POSITIVE_POINTS = check_positivity(eigenvals);
-
-  /*
-  if(VERBOSE){
-    cerr << "points = " << endl;
-    for(size_t i = 0; i < eigenvals.size(); i++)
-      cerr << eigenvals[i] << '\t';
-    cerr << endl;
-  }
-  */
-
-
-  if(POSITIVE_POINTS){
-    points.swap(eigenvals);
-    weights.swap(eigenvec);
+  const bool points_are_positive = check_positivity(eigenvals);
+  if (points_are_positive) {
+    swap(points, eigenvals);
+    swap(weights, eigenvec);
   }
 
-  for(size_t i = 0; i < weights.size(); i++)
-    weights[i] = weights[i]*weights[i];
+  // square entries in the weights vector
+  transform(begin(weights), end(weights), begin(weights),
+            [](const double x) {return x*x;});
 
-  /*
-  if(VERBOSE){
-    cerr << "weights = " << endl;
-    for(size_t i = 0; i < weights.size(); i++)
-      cerr << weights[i] << '\t';
-    cerr << endl;
-  }
-  */
-
-  return POSITIVE_POINTS;
+  return points_are_positive;
 }
