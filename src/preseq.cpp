3c264425e2f64191d59402490cd1e2455eb598ca
Andrew D Smith <andrewds@usc.edu>
2025-12-05 15:44:27 -0800

    src/preseq.cpp: fixing the args passed to subcommands

--- src/preseq.cpp
+++ src/preseq.cpp
@@ -1708,20 +1708,20 @@ main(int argc, char *argv[]) {
   if (std::strcmp(argv[1], "lc_extrap") == 0)
     return lc_extrap(argc - 1, argv + 1);
 
-  if (std::strcmp(argv[1], "c_curve") == 0)
-    return c_curve(argc - 1, argv + 1);
+  if (cmd == "lc_extrap")
+    return lc_extrap_main(argc - 1, argv + 1);
 
-  if (std::strcmp(argv[1], "gc_extrap") == 0)
-    return gc_extrap(argc - 1, argv + 1);
+  if (cmd == "c_curve")
+    return c_curve_main(argc - 1, argv + 1);
 
-  if (std::strcmp(argv[1], "bound_pop") == 0)
-    return bound_pop(argc - 1, argv + 1);
+  if (cmd == "gc_extrap")
+    return gc_extrap_main(argc - 1, argv + 1);
 
-  if (std::strcmp(argv[1], "pop_size") == 0)
-    return pop_size(argc - 1, argv + 1);
+  if (cmd == "bound_pop")
+    return bound_pop_main(argc - 1, argv + 1);
 
   if (cmd == "pop_size")
-    return pop_size_main(argc, argv);
+    return pop_size_main(argc - 1, argv + 1);
 
   std::cerr << "Error: unrecognized command: " << argv[1] << '\n'
             << usage_message() << '\n';
