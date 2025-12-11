9f60d2f201dec645981d1f45a8102f256a84cd84
Andrew D Smith <andrewds@usc.edu>
2025-12-10 19:48:03 -0800

    src/load_data_for_complexity.hcpp: some simplification

--- src/load_data_for_complexity.hpp
+++ src/load_data_for_complexity.hpp
@@ -26,16 +26,10 @@
 #include <vector>
 
 std::size_t
-load_coverage_counts_MR(const std::string &input_file_name,
-                        const std::uint32_t seed, const std::size_t bin_size,
-                        const std::size_t max_width,
-                        std::vector<double> &coverage_hist);
-
-std::size_t
-load_coverage_counts_GR(const std::string &input_file_name,
-                        const std::uint32_t seed, const std::size_t bin_size,
-                        const std::size_t max_width,
-                        std::vector<double> &coverage_hist);
+load_coverage_counts(const std::string &input_file_name,
+                     const std::uint32_t seed, const std::size_t bin_size,
+                     const std::size_t max_width,
+                     std::vector<double> &coverage_hist);
 
 std::size_t
 load_histogram(const std::string &filename, std::vector<double> &counts_hist);
@@ -45,11 +39,11 @@ load_counts(const std::string &input_file_name,
             std::vector<double> &counts_hist);
 
 std::size_t
-load_counts_BED_pe(const std::string &input_file_name,
+load_counts_bed_pe(const std::string &input_file_name,
                    std::vector<double> &counts_hist);
 
 std::size_t
-load_counts_BED_se(const std::string &input_file_name,
+load_counts_bed_se(const std::string &input_file_name,
                    std::vector<double> &counts_hist);
 
 #ifdef HAVE_HTSLIB
