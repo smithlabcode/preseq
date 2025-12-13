074facd181e11e02dfa2cdeb13644d5c9637801a
Andrew D Smith <andrewds@usc.edu>
2025-12-12 16:03:21 -0800

    src/common.hcpp: mmodernizing and fixing name of log factorial function

--- src/common.cpp
+++ src/common.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2013-2024 University of Southern California and
+/* Copyright (C) 2013-2025 University of Southern California and
  *                         Andrew D. Smith and Timothy Daley
  *
  * Authors: Timothy Daley and Andrew Smith
@@ -33,40 +33,26 @@
 #include <string>
 #include <vector>
 
-using std::array;
-using std::begin;
-using std::cbegin;
-using std::cend;
-using std::cerr;
-using std::end;
-using std::min;
-using std::mt19937;
-using std::runtime_error;
-using std::size_t;
-using std::string;
-using std::uint32_t;
-using std::vector;
+// NOLINTBEGIN(*-avoid-magic-numbers,*-narrowing-conversions,*-constant-array-index)
 
-// NOLINTBEGIN(*-avoid-magic-numbers,*-init-variables,*-narrowing-conversions,*-constant-array-index)
-
-double
-GoodToulmin2xExtrap(const vector<double> &counts_hist) {
+[[nodiscard]] auto
+GoodToulmin2xExtrap(const std::vector<double> &counts_hist) -> double {
   double two_fold_extrap = 0.0;
-  for (size_t i = 0; i < counts_hist.size(); i++)
+  for (std::size_t i = 0; i < std::size(counts_hist); ++i)
     two_fold_extrap += pow(-1.0, i + 1) * counts_hist[i];
   return two_fold_extrap;
 }
 
 // Lanczos approximation for gamma function for x >= 0.5 - essentially an
 // approximation for (x-1)!
-double
-factorial(double x) {
+[[nodiscard]] auto
+log_factorial(double x) -> double {
   // constants
   static constexpr double LogRootTwoPi = 0.9189385332046727;
   static constexpr double Euler = 2.71828182845904523536028747135;
 
   // clang-format off
-  const auto Lanczos = std::array<double, 9>{
+  static const auto Lanczos = std::array<double, 9>{
     0.99999999999980993227684700473478,
     676.520368121885098567009190444019,
     -1259.13921672240287047156078755283,
@@ -83,11 +69,11 @@ factorial(double x) {
   x -= 1.0;
 
   double Ag = Lanczos[0];
-  for (auto k = 1u; k < size(Lanczos); k++)
+  for (auto k = 1u; k < std::size(Lanczos); ++k)
     Ag += Lanczos[k] / (x + k);
 
-  const double term1 = (x + 0.5) * log((x + 7.5) / Euler);
-  const double term2 = LogRootTwoPi + log(Ag);
+  const double term1 = (x + 0.5) * std::log((x + 7.5) / Euler);
+  const double term2 = LogRootTwoPi + std::log(Ag);
 
   return term1 + (term2 - 7.0);
 }
@@ -97,32 +83,33 @@ factorial(double x) {
 // see K.L Heck 1975
 // N total sample size; S the total number of distincts
 // n sub sample size
-double
-interpolate_distinct(const vector<double> &hist, const size_t N, const size_t S,
-                     const size_t n) {
-  const double denom =
-    factorial(N + 1) - factorial(n + 1) - factorial(N - n + 1);
+[[nodiscard]] auto
+interpolate_distinct(const std::vector<double> &hist, const std::size_t N,
+                     const std::size_t S, const std::size_t n) -> double {
+  const double log_denom =
+    log_factorial(N + 1) - log_factorial(n + 1) - log_factorial(N - n + 1);
 
-  vector<double> numer(hist.size(), 0);
-  for (size_t i = 1; i < hist.size(); i++) {
+  std::vector<double> numer(hist.size(), 0);
+  for (std::size_t i = 1; i < std::size(hist); ++i) {
     // N - i -n + 1 should be greater than 0
     if (N < i + n) {
       numer[i] = 0;
     }
     else {
-      const double x =
-        (factorial(N - i + 1) - factorial(n + 1) - factorial(N - i - n + 1));
-      numer[i] = exp(x - denom) * hist[i];
+      const double x = (log_factorial(N - i + 1) - log_factorial(n + 1) -
+                        log_factorial(N - i - n + 1));
+      numer[i] = std::exp(x - log_denom) * hist[i];
     }
   }
-  return S - accumulate(cbegin(numer), cend(numer), 0);
+  return S - std::accumulate(std::cbegin(numer), std::cend(numer), 0);
 }
 
 static void
 extrapolate_curve(const ContinuedFraction &the_cf,
                   const double initial_distinct, const double vals_sum,
                   const double initial_sample_size, const double step_size,
-                  const double max_sample_size, vector<double> &estimates) {
+                  const double max_sample_size,
+                  std::vector<double> &estimates) {
   double curr_samp_sz = initial_sample_size;
   while (curr_samp_sz < max_sample_size) {
     const double fold = (curr_samp_sz - vals_sum) / vals_sum;
@@ -132,40 +119,41 @@ extrapolate_curve(const ContinuedFraction &the_cf,
   }
 }
 
-bool
+[[nodiscard]] auto
 extrap_single_estimate(const bool VERBOSE, const bool allow_defects,
-                       const vector<double> &hist, size_t max_terms,
+                       const std::vector<double> &hist, std::size_t max_terms,
                        const int diagonal, const double step_size,
                        const double max_extrap,
-                       vector<double> &yield_estimate) {
+                       std::vector<double> &yield_estimate) -> bool {
   yield_estimate.clear();
 
   const double vals_sum = get_counts_from_hist(hist);
-  const double initial_distinct = accumulate(cbegin(hist), cend(hist), 0.0);
+  const double initial_distinct =
+    std::accumulate(std::cbegin(hist), std::cend(hist), 0.0);
 
   // interpolate complexity curve by random sampling w/out replacement
-  const size_t upper_limit = vals_sum;
-  const size_t step = step_size;
-  size_t sample = static_cast<size_t>(step_size);
+  const std::size_t upper_limit = vals_sum;
+  const std::size_t step = step_size;
+  auto sample = static_cast<std::size_t>(step_size);
   for (; sample < upper_limit; sample += step)
     yield_estimate.push_back(
       interpolate_distinct(hist, upper_limit, initial_distinct, sample));
 
   // ENSURE THAT THE MAX TERMS ARE ACCEPTABLE
-  size_t first_zero = 1;
-  while (first_zero < hist.size() && hist[first_zero] > 0)
+  std::size_t first_zero = 1;
+  while (first_zero < std::size(hist) && hist[first_zero] > 0)
     ++first_zero;
 
   // Ensure we are not using a zero term
-  max_terms = min(max_terms, first_zero - 1);
+  max_terms = std::min(max_terms, first_zero - 1);
 
   // refit curve for lower bound (degree of approx is 1 less than
   // max_terms)
   max_terms = max_terms - (max_terms % 2 == 1);
 
   if (allow_defects) {
-    vector<double> ps_coeffs;
-    for (size_t j = 1; j <= max_terms; j++)
+    std::vector<double> ps_coeffs;
+    for (std::size_t j = 1; j <= max_terms; ++j)
       ps_coeffs.push_back(hist[j] * std::pow(-1.0, j + 1));
 
     const ContinuedFraction defect_cf(ps_coeffs, diagonal, max_terms);
@@ -174,7 +162,7 @@ extrap_single_estimate(const bool VERBOSE, const bool allow_defects,
                       max_extrap, yield_estimate);
 
     if (VERBOSE)
-      cerr << defect_cf << '\n';
+      std::cerr << defect_cf << '\n';
     // NO FAIL! defect mode doesn't care about failure
   }
   else {
@@ -194,7 +182,7 @@ extrap_single_estimate(const bool VERBOSE, const bool allow_defects,
     }
 
     if (VERBOSE)
-      cerr << lower_cf << '\n';
+      std::cerr << lower_cf << '\n';
   }
   // SUCCESS!!
   return true;
@@ -202,35 +190,37 @@ extrap_single_estimate(const bool VERBOSE, const bool allow_defects,
 
 void
 extrap_bootstrap(const bool VERBOSE, const bool allow_defects,
-                 const uint32_t seed, const vector<double> &orig_hist,
-                 const size_t n_bootstraps, const size_t orig_max_terms,
-                 const int diagonal, const double bin_step_size,
-                 const double max_extrap, const size_t max_iter,
-                 vector<vector<double>> &bootstrap_estimates) {
+                 const std::uint32_t seed, const std::vector<double> &orig_hist,
+                 const std::size_t n_bootstraps,
+                 const std::size_t orig_max_terms, const int diagonal,
+                 const double bin_step_size, const double max_extrap,
+                 const std::size_t max_iter,
+                 std::vector<std::vector<double>> &bootstrap_estimates) {
   // clear returning vectors
   bootstrap_estimates.clear();
 
   // setup rng
-  mt19937 rng(seed);
+  std::mt19937 rng(seed);
 
   const double initial_distinct =
-    std::accumulate(cbegin(orig_hist), cend(orig_hist), 0.0);
+    std::accumulate(std::cbegin(orig_hist), std::cend(orig_hist), 0.0);
 
-  vector<size_t> orig_hist_distinct_counts;
-  vector<double> distinct_orig_hist;
-  for (size_t i = 0; i < orig_hist.size(); i++)
+  std::vector<std::size_t> orig_hist_distinct_counts;
+  std::vector<double> distinct_orig_hist;
+  for (std::size_t i = 0; i < std::size(orig_hist); ++i)
     if (orig_hist[i] > 0) {
       orig_hist_distinct_counts.push_back(i);
       distinct_orig_hist.push_back(orig_hist[i]);
     }
 
-  for (size_t iter = 0;
-       (iter < max_iter && bootstrap_estimates.size() < n_bootstraps); ++iter) {
+  for (std::size_t iter = 0;
+       (iter < max_iter && std::size(bootstrap_estimates) < n_bootstraps);
+       ++iter) {
     if (VERBOSE && iter > 0 && iter % 72 == 0)
-      cerr << '\n';  // bootstrap success progress only 72 char wide
+      std::cerr << '\n';  // bootstrap success progress only 72 char wide
 
-    vector<double> yield_vector;
-    vector<double> hist;
+    std::vector<double> yield_vector;
+    std::vector<double> hist;
     resample_hist(rng, orig_hist_distinct_counts, distinct_orig_hist, hist);
 
     const double sample_vals_sum = get_counts_from_hist(hist);
@@ -240,8 +230,9 @@ extrap_bootstrap(const bool VERBOSE, const bool allow_defects,
       hist.pop_back();
 
     // compute complexity curve by random sampling w/out replacement
-    const size_t distinct = accumulate(cbegin(hist), cend(hist), 0.0);
-    size_t curr_sample_sz = bin_step_size;
+    const std::size_t distinct =
+      std::accumulate(std::cbegin(hist), std::cend(hist), 0.0);
+    std::size_t curr_sample_sz = bin_step_size;
     while (curr_sample_sz < sample_vals_sum) {
       yield_vector.push_back(
         interpolate_distinct(hist, sample_vals_sum, distinct, curr_sample_sz));
@@ -249,11 +240,11 @@ extrap_bootstrap(const bool VERBOSE, const bool allow_defects,
     }
 
     // ENSURE THAT THE MAX TERMS ARE ACCEPTABLE
-    size_t first_zero = 1;
-    while (first_zero < hist.size() && hist[first_zero] > 0)
+    std::size_t first_zero = 1;
+    while (first_zero < std::size(hist) && hist[first_zero] > 0)
       ++first_zero;
 
-    size_t max_terms = min(orig_max_terms, first_zero - 1);
+    std::size_t max_terms = std::min(orig_max_terms, first_zero - 1);
     // refit curve for lower bound (degree of approx is 1 less than
     // max_terms)
     max_terms = max_terms - (max_terms % 2 == 1);
@@ -261,8 +252,8 @@ extrap_bootstrap(const bool VERBOSE, const bool allow_defects,
     bool successful_bootstrap = false;
     // defect mode, simple extrapolation
     if (allow_defects) {
-      vector<double> ps_coeffs;
-      for (size_t j = 1; j <= max_terms; j++)
+      std::vector<double> ps_coeffs;
+      for (std::size_t j = 1; j <= max_terms; ++j)
         ps_coeffs.push_back(hist[j] * std::pow(-1.0, j + 1));
 
       const ContinuedFraction defect_cf(ps_coeffs, diagonal, max_terms);
@@ -293,36 +284,39 @@ extrap_bootstrap(const bool VERBOSE, const bool allow_defects,
       }
     }
     if (VERBOSE)
-      cerr << (successful_bootstrap ? '.' : '_');
+      std::cerr << (successful_bootstrap ? '.' : '_');
   }
   if (VERBOSE)
-    cerr << '\n';
-  if (bootstrap_estimates.size() < n_bootstraps)
-    throw runtime_error("too many defects in the approximation, "
-                        "consider running in defect mode");
+    std::cerr << '\n';
+  if (std::size(bootstrap_estimates) < n_bootstraps)
+    throw std::runtime_error("too many defects in the approximation, "
+                             "consider running in defect mode");
 }
 
 void
-vector_median_and_ci(const vector<vector<double>> &bootstrap_estimates,
-                     const double ci_level, vector<double> &yield_estimates,
-                     vector<double> &lower_ci_lognorm,
-                     vector<double> &upper_ci_lognorm) {
+vector_median_and_ci(
+  const std::vector<std::vector<double>> &bootstrap_estimates,
+  const double ci_level, std::vector<double> &yield_estimates,
+  std::vector<double> &lower_ci_lognorm,
+  std::vector<double> &upper_ci_lognorm) {
   yield_estimates.clear();
   lower_ci_lognorm.clear();
   upper_ci_lognorm.clear();
   assert(!bootstrap_estimates.empty());
 
-  const size_t n_est = bootstrap_estimates.size();
-  vector<double> estimates_row(n_est, 0.0);
-  for (size_t i = 0; i < bootstrap_estimates[0].size(); i++) {
+  const std::size_t n_est = std::size(bootstrap_estimates);
+  std::vector<double> estimates_row(n_est, 0.0);
+  for (std::size_t i = 0; i < std::size(bootstrap_estimates[0]); ++i) {
     // estimates is in wrong order, work locally on const val
-    for (size_t k = 0; k < n_est; ++k)
+    for (std::size_t k = 0; k < n_est; ++k)
       estimates_row[k] = bootstrap_estimates[k][i];
 
-    double median_estimate, lower_ci_estimate, upper_ci_estimate;
+    double median_estimate{};
+    double lower_ci_estimate{};
+    double upper_ci_estimate{};
     median_and_ci(estimates_row, ci_level, median_estimate, lower_ci_estimate,
                   upper_ci_estimate);
-    std::sort(begin(estimates_row), end(estimates_row));
+    std::sort(std::begin(estimates_row), std::end(estimates_row));
 
     yield_estimates.push_back(median_estimate);
     lower_ci_lognorm.push_back(lower_ci_estimate);
@@ -331,11 +325,11 @@ vector_median_and_ci(const vector<vector<double>> &bootstrap_estimates,
 }
 
 void
-write_predicted_complexity_curve(const string &outfile, const double c_level,
-                                 const double step_size,
-                                 const vector<double> &yield_estimates,
-                                 const vector<double> &yield_lower_ci_lognorm,
-                                 const vector<double> &yield_upper_ci_lognorm) {
+write_predicted_complexity_curve(
+  const std::string &outfile, const double c_level, const double step_size,
+  const std::vector<double> &yield_estimates,
+  const std::vector<double> &yield_lower_ci_lognorm,
+  const std::vector<double> &yield_upper_ci_lognorm) {
   std::ofstream of;
   if (!outfile.empty())
     of.open(outfile);
@@ -352,7 +346,7 @@ write_predicted_complexity_curve(const string &outfile, const double c_level,
   out.precision(1);
 
   out << 0 << '\t' << 0 << '\t' << 0 << '\t' << 0 << '\n';
-  for (size_t i = 0; i < yield_estimates.size(); ++i)
+  for (std::size_t i = 0; i < std::size(yield_estimates); ++i)
     out << (i + 1) * step_size << '\t' << yield_estimates[i] << '\t'
         << yield_lower_ci_lognorm[i] << '\t' << yield_upper_ci_lognorm[i]
         << '\n';
@@ -364,32 +358,33 @@ write_predicted_complexity_curve(const string &outfile, const double c_level,
 // distinct_counts_hist[k] = vals_hist[vals_hist_distinct_counts[k]]
 // stores the kth positive value of vals_hist
 void
-resample_hist(mt19937 &gen, const vector<size_t> &vals_hist_distinct_counts,
-              const vector<double> &distinct_counts_hist,
-              vector<double> &out_hist) {
-  const size_t hist_size = distinct_counts_hist.size();
-  vector<uint32_t> sample_distinct_counts_hist(hist_size, 0);
+resample_hist(std::mt19937 &gen,
+              const std::vector<std::size_t> &vals_hist_distinct_counts,
+              const std::vector<double> &distinct_counts_hist,
+              std::vector<double> &out_hist) {
+  const std::size_t hist_size = std::size(distinct_counts_hist);
+  std::vector<std::uint32_t> sample_distinct_counts_hist(hist_size, 0);
 
-  const uint32_t distinct =
-    accumulate(cbegin(distinct_counts_hist), cend(distinct_counts_hist), 0.0);
+  const std::uint32_t distinct = std::accumulate(
+    std::cbegin(distinct_counts_hist), std::cend(distinct_counts_hist), 0.0);
 
   multinomial(gen, distinct_counts_hist, distinct, sample_distinct_counts_hist);
 
   out_hist.clear();
   out_hist.resize(vals_hist_distinct_counts.back() + 1, 0.0);
-  for (size_t i = 0; i < hist_size; i++)
+  for (std::size_t i = 0; i < hist_size; ++i)
     out_hist[vals_hist_distinct_counts[i]] = sample_distinct_counts_hist[i];
 }
 
 template <typename T>
-T
-median_from_sorted_vector(const vector<T> &sorted_data, const size_t stride,
-                          const size_t n) {
+[[nodiscard]] auto
+median_from_sorted_vector(const std::vector<T> &sorted_data,
+                          const std::size_t stride, const std::size_t n) -> T {
   if (n == 0 || sorted_data.empty())
     return 0.0;
 
-  const size_t lhs = (n - 1) / 2;
-  const size_t rhs = n / 2;
+  const std::size_t lhs = (n - 1) / 2;
+  const std::size_t rhs = n / 2;
 
   if (lhs == rhs)
     return sorted_data[lhs * stride];
@@ -398,11 +393,12 @@ median_from_sorted_vector(const vector<T> &sorted_data, const size_t stride,
 }
 
 template <typename T>
-T
-quantile_from_sorted_vector(const vector<T> &sorted_data, const size_t stride,
-                            const size_t n, const double f) {
+[[nodiscard]] auto
+quantile_from_sorted_vector(const std::vector<T> &sorted_data,
+                            const std::size_t stride, const std::size_t n,
+                            const double f) -> T {
   const double index = f * (n - 1);
-  const size_t lhs = static_cast<int>(index);
+  const std::size_t lhs = static_cast<int>(index);
   const double delta = index - lhs;
 
   if (n == 0 || sorted_data.empty())
@@ -417,15 +413,14 @@ quantile_from_sorted_vector(const vector<T> &sorted_data, const size_t stride,
 
 // Confidence interval stuff
 void
-median_and_ci(vector<double> estimates,  // by val so we can sort them
+median_and_ci(std::vector<double> estimates,  // by val so we can sort them
               const double ci_level, double &median_estimate,
               double &lower_ci_estimate, double &upper_ci_estimate) {
   assert(!estimates.empty());
-
-  std::sort(begin(estimates), end(estimates));
+  std::sort(std::begin(estimates), std::end(estimates));
 
   const double alpha = 1.0 - ci_level;
-  const size_t N = estimates.size();
+  const std::size_t N = std::size(estimates);
 
   median_estimate = median_from_sorted_vector(estimates, 1, N);
   lower_ci_estimate = quantile_from_sorted_vector(estimates, 1, N, alpha / 2);
@@ -433,4 +428,4 @@ median_and_ci(vector<double> estimates,  // by val so we can sort them
     quantile_from_sorted_vector(estimates, 1, N, 1.0 - alpha / 2);
 }
 
-// NOLINTEND(*-avoid-magic-numbers,*-init-variables,*-narrowing-conversions,*-constant-array-index)
+// NOLINTEND(*-avoid-magic-numbers,*-narrowing-conversions,*-constant-array-index)
