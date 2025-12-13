de7f45f7c915a8b0b4dda655717b81dd0dbbfa98
Andrew D Smith <andrewds@usc.edu>
2025-12-13 13:31:10 -0800

    src/moment_sequence.hcpp: formatting

--- src/moment_sequence.hpp
+++ src/moment_sequence.hpp
@@ -1,20 +1,20 @@
-/*    Copyright (C) 2013 University of Southern California and
- *                       Andrew D. Smith and Timothy Daley
+/* Copyright (C) 2013 University of Southern California and
+ *                    Andrew D. Smith and Timothy Daley
  *
- *    Authors: Andrew D. Smith and Timothy Daley
+ * Authors: Andrew D. Smith and Timothy Daley
  *
- *    This program is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
  *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
  *
- *    You should have received a copy of the GNU General Public License
- *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef SRC_MOMENT_SEQUENCE_HPP_
@@ -26,15 +26,14 @@
 // test Hankel moment matrix to ensure the moment sequence
 // is positive definite
 std::size_t
-ensure_pos_def_mom_seq(std::vector<double> &moments, const double tolerance,
-                       const bool VERBOSE);
+ensure_pos_def_mom_seq(std::vector<double> &moments, const double tolerance);
 
 struct MomentSequence {
   MomentSequence() {}
   explicit MomentSequence(const std::vector<double> &obs_moms);
 
-  MomentSequence(const std::vector<double> &a, const std::vector<double> &b) :
-    alpha(a), beta(b) {}
+  MomentSequence(const std::vector<double> &alpha,
+                 const std::vector<double> &beta) : alpha{alpha}, beta{beta} {}
 
   // Estimate 3-term recurrence
   // these will be removed from the header when they are tested
@@ -47,7 +46,7 @@ struct MomentSequence {
 
   // quadrature rules using QR on Jacobi matrix
   bool
-  Lower_quadrature_rules(const std::size_t n_points, const double tolerance,
+  lower_quadrature_rules(const std::size_t n_points, const double tolerance,
                          const std::size_t max_iter,
                          std::vector<double> &points,
                          std::vector<double> &weights);
