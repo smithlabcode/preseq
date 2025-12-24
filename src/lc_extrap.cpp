041de2b378815c301aa000ebb22446e116f79ec1
Andrew D Smith <andrewds@usc.edu>
2025-12-23 19:29:54 -0800

    linting

--- src/lc_extrap.cpp
+++ src/lc_extrap.cpp
@@ -66,7 +66,10 @@ lc_extrap::main(int argc, char *argv[]) -> int {  // NOLINT(*-avoid-c-arrays)
     bool single_estimate{false};
     bool allow_defects{false};
 
+#ifdef HAVE_HTSLIB
     std::uint32_t n_threads{1};
+#endif
+
     CLI::App app{rlstrip(about_msg)};
     argv = app.ensure_utf8(argv);
     app.formatter(std::make_shared<preseq_formatter>());
@@ -93,8 +96,7 @@ lc_extrap::main(int argc, char *argv[]) -> int {  // NOLINT(*-avoid-c-arrays)
     app.add_option("-x,--terms", orig_max_terms, "maximum terms in estimator");
     app.add_option("-r,--seed", seed, "seed for random number generator");
     app.add_flag("-p,--paired-end", paired_end, "input is paired end read file");
-    app.add_flag("-Q,--quick", single_estimate,
-                 "do not use bootstraps for confidence intervals");
+    app.add_flag("-q,--quick", single_estimate, "no bootstraps for confidence intervals");
     app.add_flag("-D,--defects", allow_defects, "no testing for defects");
     app.add_flag("-v,--verbose", verbose, "print more info");
     // clang-format on
