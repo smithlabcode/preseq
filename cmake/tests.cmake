9efc05fbe58d8ef333527c03f85e1208b4a9f3f9
Andrew D Smith <andrewds@usc.edu>
2026-08-25 13:38:21 -0700

    cmake/tests.cmake: adding test for lc_extrap with vals input

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -22,3 +22,4 @@ find_program(BASH_PROGRAM bash)
 
 ## Add each test
 add_test(NAME "lc_extrap histogram input" COMMAND bash test/lc_extrap_hist.sh)
+add_test(NAME "lc_extrap values input" COMMAND bash test/lc_extrap_vals.sh)
