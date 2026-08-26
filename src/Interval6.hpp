026a47d115180db29fa89d64dc6bc7d61c3f8838
Andrew D Smith <andrewds@usc.edu>
2026-08-26 12:04:57 -0700

    src/Interval6.hpp: update for c++20

--- src/Interval6.hpp
+++ src/Interval6.hpp
@@ -18,9 +18,10 @@
 #ifndef SRC_INTERVAL6_HPP_
 #define SRC_INTERVAL6_HPP_
 
+#include <compare>
 #include <cstdint>
-//  #include <format> // ADS: needs c++20
-#include <iterator>  // std::size
+#include <format>
+#include <iterator>
 #include <stdexcept>
 #include <string>
 #include <vector>
@@ -37,8 +38,8 @@ struct Interval6 {
   Interval6(const std::string &chrom, const std::uint32_t start,
             const std::uint32_t stop, const std::string &name,
             const double score, const char strand) :
-    chrom{chrom}, start{start}, stop{stop}, name{name}, score{score},
-    strand{strand} {}
+    chrom{chrom},
+    start{start}, stop{stop}, name{name}, score{score}, strand{strand} {}
 
   explicit Interval6(const std::string &line) {
     if (!initialize(line.data(), line.data() + std::size(line)))
@@ -54,8 +55,8 @@ struct Interval6 {
              (start < rhs.start || (start == rhs.start && stop < rhs.stop))));
   }
 
-  // auto
-  // operator<=>(const Interval6 &) const = default;
+  auto
+  operator<=>(const Interval6 &) const = default;
 };
 
 [[nodiscard]] inline auto
@@ -65,17 +66,15 @@ to_string(const Interval6 &x) -> std::string {
          std::to_string(x.score) + "\t" + std::string(1, x.strand);
 }
 
-// ADS: need to bump to c++20 for this
-//
-// template <> struct std::formatter<Interval6> : std::formatter<std::string> {
-//   auto
-//   format(const Interval6 &i, format_context &ctx) const {
-//     static constexpr auto fmt = "{}\t{}\t{}\t{}\t{:.6g}\t{}";
-//     return std::formatter<std::string>::format(
-//       std::format(fmt, i.chrom, i.start, i.stop, i.name, i.score, i.strand),
-//       ctx);
-//   }
-// };
+template <> struct std::formatter<Interval6> : std::formatter<std::string> {
+  auto
+  format(const Interval6 &i, format_context &ctx) const {
+    static constexpr auto fmt = "{}\t{}\t{}\t{}\t{:.6g}\t{}";
+    return std::formatter<std::string>::format(
+      std::format(fmt, i.chrom, i.start, i.stop, i.name, i.score, i.strand),
+      ctx);
+  }
+};
 
 [[nodiscard]] inline auto
 size(const Interval6 &x) {
