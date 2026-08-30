c901a519a43afcaacee04c98e7a762e726ea205f
Andrew D Smith <andrewds@usc.edu>
2026-08-30 16:19:22 -0700

    src/preseq.cpp: updates to reflect the changes in each of the individual command source files

--- src/preseq.cpp
+++ src/preseq.cpp
@@ -1,22 +1,19 @@
 /* preseq: a tool for analyzing sequencing library complexity
  *
- * Copyright (C) 2013-2025 University of Southern California and
- *                         Andrew D. Smith and Timothy Daley
+ * Copyright (C) 2013-2026 Andrew D. Smith and Timothy Daley
  *
- * Author: Andrew D Smith
- *
- * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "bound_pop.hpp"
@@ -35,8 +32,6 @@
 #include <memory>
 #include <print>
 #include <string>
-#include <string_view>
-#include <vector>
 
 #ifdef INCLUDE_FULL_LICENSE_INFO
 #include <license.h>
@@ -71,11 +66,11 @@ main(int argc, char *argv[]) {  // NOLINT(*-c-arrays)
 #ifdef INCLUDE_FULL_LICENSE_INFO
   app.add_flag("--licenses", print_licenses, "view licenses");
 #endif
-  const auto lc_extrap_cmd = app.add_subcommand("lc_extrap", rlstrip(lc_extrap::about_msg));
-  const auto gc_extrap_cmd = app.add_subcommand("gc_extrap", rlstrip(gc_extrap::about_msg));
-  const auto pop_size_cmd = app.add_subcommand("pop_size", rlstrip(pop_size::about_msg));
-  const auto bound_pop_cmd = app.add_subcommand("bound_pop", rlstrip(bound_pop::about_msg));
-  const auto c_curve_cmd = app.add_subcommand("c_curve", rlstrip(c_curve::about_msg));
+  const auto lc_extrap_cmd = app.add_subcommand("lc_extrap", rlstrip(lc_extrap_about_msg));
+  const auto gc_extrap_cmd = app.add_subcommand("gc_extrap", rlstrip(gc_extrap_about_msg));
+  const auto pop_size_cmd = app.add_subcommand("pop_size", rlstrip(pop_size_about_msg));
+  const auto bound_pop_cmd = app.add_subcommand("bound_pop", rlstrip(bound_pop_about_msg));
+  const auto c_curve_cmd = app.add_subcommand("c_curve", rlstrip(c_curve_about_msg));
   // clang-format on
 
   if (argc < 2) {
@@ -85,31 +80,31 @@ main(int argc, char *argv[]) {  // NOLINT(*-c-arrays)
   CLI11_PARSE(app, argc, argv);
 
   if (print_version) {
-    std::cout << VERSION << '\n';
+    std::println("{}", VERSION);
     return EXIT_SUCCESS;
   }
 
 #ifdef INCLUDE_FULL_LICENSE_INFO
   if (view_licenses) {
-    std::cout << license_text;
+    std::println("{}", license_text);
     return EXIT_SUCCESS;
   }
 #endif
 
   if (app.got_subcommand(lc_extrap_cmd))
-    return lc_extrap::main(argc - 1, argv + 1);
+    return lc_extrap_main(argc - 1, argv + 1);
 
   if (app.got_subcommand(gc_extrap_cmd))
-    return gc_extrap::main(argc - 1, argv + 1);
+    return gc_extrap_main(argc - 1, argv + 1);
 
   if (app.got_subcommand(c_curve_cmd))
-    return c_curve::main(argc - 1, argv + 1);
+    return c_curve_main(argc - 1, argv + 1);
 
   if (app.got_subcommand(pop_size_cmd))
-    return pop_size::main(argc - 1, argv + 1);
+    return pop_size_main(argc - 1, argv + 1);
 
   if (app.got_subcommand(bound_pop_cmd))
-    return bound_pop::main(argc - 1, argv + 1);
+    return bound_pop_main(argc - 1, argv + 1);
 
   // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
   std::println(std::cerr, "unrecognized command: {}", argv[1]);
